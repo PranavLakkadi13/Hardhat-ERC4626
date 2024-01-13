f1cb3210e9b989401530ed84f7a1a2c1609e7141
Pranav <pranavlakkadi@gmail.com>
2024-01-14 03:19:59 +0530

    added code

---
+++ src/Treasury.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Treasury is Ownable {
+    address private i_owner;
+
+    constructor () {
+        i_owner = msg.sender;
+    }
+
+    function withdraw() external {
+        require(msg.sender == i_owner);
+
+        (bool ok, ) = i_owner.call{value: address(this).balance }("");
+        require(ok);
+    }
+
+    function allowance(address _asset) external {
+        require(msg.sender == i_owner);
+
+        IERC20(_asset).approve(i_owner,IERC20(_asset).balanceOf(address(this)));
+    }
+}
---
+++ test/Treasury.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Treasury} from "../src/Treasury.sol";
+
+contract TreasuryTest is Test {
+    
+}
