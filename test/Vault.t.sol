1954bcf6a308a4fa29329ab255cd9995cfa5f5e1
Pranav <pranavlakkadi@gmail.com>
2024-01-15 02:24:04 +0530

    changes

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {Asset} from "../src/Asset.sol";
 import {VaultContract} from "../src/VaultContract.sol";
 
@@ -51,4 +51,80 @@ contract VaultTest is Test {
         assertEq(vault.totalSupply(),10e18);
     }
 
+    function testTokenShareMints() public {
+        assertEq(vault.balanceOf(bob),0);
+        
+        vm.prank(bob);
+        asset.approve(address(vault), 10e18);
+
+        vm.prank(bob);
+        uint256 x = vault.deposit(10e18, bob);
+
+        
+        // console.log("The amount of shares minted " , x);
+
+        assertEq(vault.balanceOf(bob), x);
+    }
+
+    function testTokenShareMints2() public {
+        assertEq(vault.balanceOf(bob),0);
+        
+        vm.prank(bob);
+        asset.approve(address(vault), 20e18);
+
+        vm.prank(bob);
+        uint256 x = vault.deposit(10e18, bob);
+
+        
+        // console.log("The amount of shares minted " , x);
+
+        assertEq(vault.balanceOf(bob), x);
+
+        assertEq(vault.totalSupply(), 10e18);
+
+        vm.prank(bob);
+        uint256 y = vault.deposit(10e18, bob);
+
+        // console.log("The amount of shares minted " , y);
+
+        assertEq(vault.balanceOf(bob), y + x);
+        assertEq(vault.totalSupply(),20e18);
+    }
+
+    function testredeemTokens() public {
+        vm.startPrank(bob);
+        asset.approve(address(vault), 20e18);
+        
+        vault.deposit(10e18, bob);
+
+        assertEq(vault.totalSupply(),10e18);
+
+        vault.withdraw(10e18, bob, bob);
+
+        vm.stopPrank();
+
+        assertEq(vault.totalAssets(),0);
+    }
+
+    function testRedeemfunction() public {
+        vm.startPrank(bob);
+        asset.approve(address(vault), 20e18);
+        
+        vault.deposit(10e18, bob);
+
+        assertEq(vault.totalSupply(),10e18);
+
+        vault.redeem(10e18, bob, bob);
+
+        vm.stopPrank();
+        assertEq(vault.totalAssets(),0);
+    }
+
+    function testGlobalStateValues() public {
+        assertEq(vault.asset(), address(asset));
+        assertEq(vault.decimals(),18);
+        assertEq(vault.name(), "Vault Token");
+        assertEq(vault.symbol(), "VLT");
+    }
+    
 }
